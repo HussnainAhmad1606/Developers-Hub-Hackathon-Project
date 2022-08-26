96cc31ef099b5c84f37dc6d881845a10c1d69a7a
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2022-08-25 21:47:27 -0700

    CRUD Application & Authentication System
    Add Django Authentication System & create CRUD application by using HarperDB

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name="homepage"),
+    path('developers/', views.developers, name="developers"),
+    path('developers/add/', views.add_developer, name="add_developer"),
+    path('developers/delete/<str:pk>', views.delete_developer, name="delete_developer"),
+    path('developers/update/<str:pk>', views.update_developer, name="update_developer"),
+    path('developers/profile/<str:pk>/', views.dev_profile, name="dev_profile"),
+    path('login/', views.handleLogin, name="login"),
+    path('signup/', views.handleSignup, name="signup"),
+    path('logout/', views.handlelogout, name="logout"),
+
+]
---
+++ core/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.conf import settings
+# Create your views here.
+
+db = settings.DB
+
+def home(request):
+    return render(request, "core/index.html")
+
+
+def developers(request):
+    devs = db.search_by_value('hackathon', 'developers', "id", "*", get_attributes=['*'])
+    context = {"devs":devs}
+    return render(request, 'core/developers.html', context)
+
+def dev_profile(request, pk):
+    dev = db.search_by_hash('hackathon', 'developers', [pk], get_attributes=['*'])[0]
+    context = {"dev":dev}
+    return render(request, 'core/dev_profile.html', context)
+
+def add_developer(request):
+    if request.method == "POST":
+        data = request.POST
+        db.insert('hackathon', 'developers', [{"name":data['name'], "skills":data['skills']}])
+        messages.success(request, "Developer Profile Added Successfully")
+        return redirect('homepage')
+    return render(request, 'core/add_developer.html')
+
+def update_developer(request, pk):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        skills = request.POST.get("skills")
+        db.update('hackathon', 'developers', [{"id":pk,"name":name,"skills":skills}])
+        messages.success(request, "Profile Updated Successfully")
+        return redirect('homepage')
+
+    dev = db.search_by_hash('hackathon', 'developers', [pk], get_attributes=['*'])[0]
+    context = {"dev":dev}
+    return render(request, 'core/update_developer.html', context)
+
+def delete_developer(request, pk):
+    if request.method == "POST":
+        db.delete('hackathon', 'developers', [pk])
+        messages.success(request, "Developer Profile Deleted Successfully")
+        return redirect("developers")
+
+def handleLogin(request):
+    if request.method == "POST":
+        data = request.POST
+        user = authenticate(username=data["username"], password=data["password"])
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in Successfully")
+            return redirect("homepage")
+        else:
+            messages.error(request, "Error! Make sure your credientials are correct & Try Again")
+
+    return render(request, "core/login.html")
+
+
+def handleSignup(request):
+    if request.method == "POST":
+        data = request.POST
+        print(data["username"])
+        if User.objects.filter(username=data["username"]).exists():
+            messages.info(request, 'Username is already taken please choose different')
+            return redirect("signup")
+        elif User.objects.filter(email=data["email"]).exists():
+            messages.info(request, 'Email is already taken')
+            return redirect("signup")
+        else:
+            newUser = User.objects.create_user(data["username"], data["email"], data["password"])
+            newUser.save()
+            messages.success(request, "Account Created Successfully. You can login now.")
+            return redirect("homepage")
+
+    return render(request, "core/signup.html")
+
+def handlelogout(request):
+    if request.method == "POST":
+        logout(request)
+        messages.success(request, "Account Logout Success")
+        return redirect("homepage")
